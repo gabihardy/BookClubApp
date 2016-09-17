b4c2e3c88edd3586d37c5b6a66233d64257cc3bc
Gabrielle Hardy <gabrhardy@gmail.com>
2016-09-17 11:30:18 -0500

    having git troubles what is going on

---
+++ app.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+	var queryURLBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + authKey + "&q=";
+	var authKey = "a5ed8a26fd7c4b7e97c628bb78c75299"
+	console.log(URL : queryURLBase)
+});
