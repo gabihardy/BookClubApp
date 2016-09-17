35c714e2faa9062fa3cb10d889477bf2993ca9ba
Gabrielle Hardy <gabrhardy@gmail.com>
2016-09-17 11:39:15 -0500

    omg

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
 	var queryURLBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + authKey + "&q=";
-	var authKey = "a5ed8a26fd7c4b7e97c628bb78c75299"
-	console.log(URL : queryURLBase)
+	var authKey = "a5ed8a26fd7c4b7e97c628bb78c75299";
+	console.log(URL : queryURLBase);
+	
+	$('#runSearch').on('click', function(){
+		var searchTerm = $('#searchTerm').val().trim();
+		queryURL = queryURLBase + searchTerm;
+	});
+
 });
