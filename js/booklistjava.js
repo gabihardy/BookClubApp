856f339b40703abef1e6a3e0b6817672ded4ab2f
John Overton <John Overton>
2016-10-05 17:12:02 -0500

    clean code just

--- js/booklistjava.js
+++ js/booklistjava.js
@@ -1,27 +1,28 @@
-var books = ['Atlas Shrugged','Gone Girl', 'the catcher in the rye'];
-
-for (var i =  0; i < books.length; i++) {
-	console.log(books[i]);
-
-
-var queryURL = "http://openlibrary.org/search.json?q=" + books[i];
-
-var authKey = "dc3d0abdcc52a4411782f3b4d54ab0322a816e1a";
-
-
-
-$.ajax({url: queryURL, method: 'GET', data: {limit: 1}, dataType: "json"}).done(function(response) {
-				console.log(response);
-				 console.log(response.docs[0].author_name[0]);
-				 console.log(response.docs[0].isbn[0]);
-				console.log(response.docs[0].title);
-				console.log(response.docs.cover_edition_key);
-				
-				$("#previousBookView").append("<img src=http://covers.openlibrary.org/b/isbn/" + response.docs[0].isbn[0] + "-M.jpg />");
-				$("#previousBookView").append("<div>" + (response.docs[0].author_name[0]) + "</>");
-				$("#previousBookView").append("<div>" + (response.docs[0].title) + "</>");
-
-});
-
-}
+var books = ['Atlas Shrugged', 'Gone Girl', 'the catcher in the rye'];
+ for (var i = 0; i < books.length; i++) {
+     console.log(books[i]);
+     var queryURL = "http://openlibrary.org/search.json?q=" + books[i];
+     var authKey = "dc3d0abdcc52a4411782f3b4d54ab0322a816e1a";
+     $.ajax({
+         url: queryURL,
+         method: 'GET',
+         data: {
+             limit: 1
+         },
+         dataType: "json"
+     }).done(function(response) {
+         console.log(response);
+         console.log(response.docs[0].author_name[0]);
+         console.log(response.docs[0].isbn[0]);
+         console.log(response.docs[0].title);
+         console.log(response.docs.cover_edition_key);
+         $("#previousBookView").append(
+             "<img src=http://covers.openlibrary.org/b/isbn/" +
+             response.docs[0].isbn[0] + "-M.jpg />");
+         $("#previousBookView").append("<div>" + (response.docs[0].author_name[
+             0]) + "</>");
+         $("#previousBookView").append("<div>" + (response.docs[0].title) +
+             "</>");
+     });
+ }
 
--- js/javascript.js
+++ js/javascript.js
@@ -1,148 +1,77 @@
+// Hides form that adds book search to members
  $(document).ready(function() {
-    $('#form').hide();
+     $('#form').hide();
+     // Initialize Firebase
+     var config = {
+         apiKey: "AIzaSyBWoGRlIJelSKmejkb5R9QiAZ1oZItNwVY",
+         authDomain: "bookclubapp-df393.firebaseapp.com",
+         databaseURL: "https://bookclubapp-df393.firebaseio.com",
+         storageBucket: "bookclubapp-df393.appspot.com",
+         messagingSenderId: "479342044608"
+     };
+     firebase.initializeApp(config);
+     var database = firebase.database();
+     // Code that fills the table of members
+     database.ref("/members").orderByChild("joinDate").on("child_added",
+         function(snapshot) {
+             console.log(snapshot.key);
+             var firstName = snapshot.val().firstName;
+             var lastName = snapshot.val().lastName;
+             var email = snapshot.val().email;
+             var address = snapshot.val().address;
+             var city = snapshot.val().city;
+             var state = snapshot.val().state;
+             var zip = snapshot.val().zip;
+             var phone = snapshot.val().phone;
+             var joinDate = snapshot.val().joinDate;
+             var book = snapshot.val().book;
+             var key = snapshot.val().key
+             var memberTable = $("#memberTable");
+             var tableRow = $("<tr>");
 
-    // Initialize Firebase
-    var config = {
-        apiKey: "AIzaSyBWoGRlIJelSKmejkb5R9QiAZ1oZItNwVY",
-        authDomain: "bookclubapp-df393.firebaseapp.com",
-        databaseURL: "https://bookclubapp-df393.firebaseio.com",
-        storageBucket: "bookclubapp-df393.appspot.com",
-        messagingSenderId: "479342044608"
-    };
-    firebase.initializeApp(config);
+             tableRow.append("<td>" + moment(joinDate).add(730,'days').format("MM/DD/YY") + "</td");
+             tableRow.append("<td>" + firstName + " " + lastName +"</td>");
+             tableRow.append("<td>" + phone.replace(/(\d{3})(\d{3})(\d{4})/, '$1-$2-$3') + "</td>");
+             tableRow.append("<td>" + book + "</td>");
+             memberTable.append(tableRow);
+         }, function(errorObject) {
+             console.log("Error handled: " + errorObject.code);
+         });
+     // adds click listeners to table to add book choice to memeber work in progress I want to have this working next commit
 
-    var database = firebase.database();
+     $('table').on('click', 'tr', function() {
+         console.log(this);
+         $(this).attr("data-key");
+         $('#form').show();
+         $("#addBookBtn").on("click", function(event) {
+             $('#form').hide();
+             event.preventDefault();
+             var book = $('#bookSearch').val().trim();
 
-    database.ref("/members").orderByChild("joinDate").on("child_added", function(snapshot) {
+             database.ref("/books").push({
+                book: book             
+                });
 
-        // console.log(snapshot.val());
-        var firstName = snapshot.val().firstName;
-        var lastName = snapshot.val().lastName;
-        var email = snapshot.val().email;
-        var address = snapshot.val().address;
-        var city = snapshot.val().city;
-        var state = snapshot.val().state;
-        var zip = snapshot.val().zip;
-        var phone = snapshot.val().phone;
-        var joinDate = snapshot.val().joinDate;
-        var book = snapshot.val().book;
-
-        
-        var memberTable = $("#memberTable");
-        var tableRow = $("<tr>");
-        tableRow.append("<td>" + moment(joinDate).format("MM/DD/YY") + "</td")
-        tableRow.append("<td>" + firstName + " " + lastName +"</td>");
-        tableRow.append("<td>" + phone.replace(/(\d{3})(\d{3})(\d{4})/,'$1-$2-$3') + "</td>");
-        tableRow.append("<td>" + book + "</td>");
-        
-        memberTable.append(tableRow);
-    }, function(errorObject) {
-        console.log("Error handled: " + errorObject.code);
-
-
-    });
-
-    $('table').on('click', 'tr', function() {
-        console.log(this);
-       $(this).attr("data-key");
-            
-        $('#form').show();
-        
-
-
-
-        $("#addBookBtn").on("click", function(event) {
-               $('#form').hide();
-                event.preventDefault();
-
-
-            
-            
-            
-
-        
-// database.ref("/members").orderByChild("joinDate").on("child_added", function(snapshot) {
-            
-            var book = $('#bookSearch').val();
-            console.log('book');
-            var queryURL = "http://openlibrary.org/search.json?q=Atlas+Shrugged";            
-            
-
-
-
-            $.ajax({
-                url: queryURL,
-                method: 'GET',
-                data: {
-                    limit: 1
-                },
-                dataType: "json"
-            }).done(function(response) {
-                console.log(response);
-                console.log(response.docs[0].author_name[0]);
-                console.log(response.docs[0].isbn[0]);
-                console.log(response.docs[0].title);
-                console.log(response.docs.cover_edition_key);
-
-                $("#featuredBookCover").append("<img src=http://covers.openlibrary.org/b/isbn/" + response.docs[0].isbn[0] + "-M.jpg />");
-                $("#featuredBookAuthor").append("<div>" + (response.docs[0].author_name[0]) + "</>");
-               
-
-
-            // var book = $('#bookSearch').val().trim();
-
-            // var firstName = snapshot.val().firstName;
-            // var lastName = snapshot.val().lastName;
-            // var email = snapshot.val().email;
-            // var address = snapshot.val().address;
-            // var city = snapshot.val().city;
-            // var state = snapshot.val().state;
-            // var zip = snapshot.val().zip;
-            // var phone = snapshot.val().phone;
-            // var joinDate = snapshot.val().joinDate;
-            // var book = snapshot.val().book;            
-
-            // var memberTable = $("#memberTable");
-            // var tableRow = $("<tr>");
-            // // tableRow.append("<td>" + moment(joinDate).format("MM/DD/YY") + "</td")
-            // // tableRow.append("<td>" + firstName + " " + lastName +"</td>");
-            // // tableRow.append("<td>" + phone.replace(/(\d{3})(\d{3})(\d{4})/,'$1-$2-$3') + "</td>");
-            // // tableRow.append("<td>" + book + "</td>");   
-
-            // memberTable.prepend(tableRow);
-
-
-
-
-         
-       
-            // database.ref("/members").push({
-            //     joinDate: joinDate,
-            //     firstName: firstName,
-            //     lastName: lastName,
-            //     email: email,
-            //     address: address,
-            //     city: city,
-            //     state: state,
-            //     zip: zip,
-            //     phone: phone,
-            //     book: book
-            // });
+              // sends book search to open library api to pull data 
+             var book = $('#bookSearch').val().trim();
+             var queryURLBase = "http://openlibrary.org/search.json?q=";
+             var queryURL = queryURLBase + book;
+             $.ajax({
+                 url: queryURL,
+                 method: 'GET',
+                 data: {
+                     limit: 1
+                 },
+                 dataType: "json"
+             }).done(function(response) {
+                 console.log(response);
+                 console.log(response.docs[0].author_name[0]);
+                 console.log(response.docs[0].isbn[0]);
+                 console.log(response.docs[0].title);
+                 console.log(response.docs.cover_edition_key);
+                 $("#featuredBookCover").append("<img src=http://covers.openlibrary.org/b/isbn/" + response.docs[0].isbn[0] + "-M.jpg />");
+                 $("#featuredBookAuthor").append("<div>" + (response.docs[0].author_name[0]) + "</>");
              });
-
-// });
-            
-            });
-
-    });
-
-});
-
-
-
-
-
-        
-	
-
-
+         });
+     });
+ });
